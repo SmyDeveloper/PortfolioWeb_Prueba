ae9c49fde4c2c12dcdc03da8da7d65dbd1fe2979
SmyDeveloper <smailliwarrillaga@gmail.com>
2023-01-03 01:27:25 -0300

    footer, proyecto, pequeñas modificaciones

--- ProyectoIntegradorAP/src/app/app.module.ts
+++ ProyectoIntegradorAP/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardysoftComponent } from './components/hardysoft/hardysoft.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { HardysoftComponent } from './components/hardysoft/hardysoft.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HardysoftComponent
+    HardysoftComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
