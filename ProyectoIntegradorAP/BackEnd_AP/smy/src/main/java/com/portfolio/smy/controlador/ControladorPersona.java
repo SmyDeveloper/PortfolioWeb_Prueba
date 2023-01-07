0f12205cd260ca0d761a9f4bd383fced14eb2a86
SmyDeveloper <smailliwarrillaga@gmail.com>
2023-01-07 16:49:07 -0300

    implementcion front

--- ProyectoIntegradorAP/BackEnd_AP/smy/src/main/java/com/portfolio/smy/controlador/ControladorPersona.java
+++ ProyectoIntegradorAP/BackEnd_AP/smy/src/main/java/com/portfolio/smy/controlador/ControladorPersona.java
@@ -5,6 +5,7 @@ import com.portfolio.smy.entidad.persona;
 import com.portfolio.smy.interfaces.IPersonaServicios;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ControladorPersona {
     @Autowired IPersonaServicios ipersonaServicios;
     
@@ -49,4 +51,10 @@ public class ControladorPersona {
         ipersonaServicios.savePersona(persona);
         return persona;
     }
+    
+    @GetMapping("/personas/traer/perfil")
+    public persona findPersona(){
+        return ipersonaServicios.findPersona((long)2);
+    }
+    
 }
